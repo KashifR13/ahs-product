c53c81789f1e56f5c873a37c62c22da3661a409f
Kashif Rasool <kashif.rasool@tuni.fi>
2023-02-11 11:48:29 +0200

    Setup and load maven

---
+++ src/test/java/com/alhadi/ahsproduct/AhsProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alhadi.ahsproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhsProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
